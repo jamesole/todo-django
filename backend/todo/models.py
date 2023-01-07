98ba261e68dd1b4b3b106a63ca3c02171922bd05
jamesole <87213195+jamesole@users.noreply.github.com>
2023-01-07 11:24:49 -0500

    Create models.py

---
+++ backend/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200, default=None, blank=False)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
